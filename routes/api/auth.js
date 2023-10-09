4a4f3382cbf681cca17811e8612e55a7f36a722b
Max <geramaks03@gmail.com>
2023-10-09 17:39:30 +0300

    Finish

---
+++ routes/api/auth.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const router = express.Router();
+
+const {authentificate, upload} = require('../../middlewares');
+const { validateBody } = require("../../helpers");
+const { schemas } = require("../../models/user");
+const ctrlWrapper = require("../../controllers/auth-controllers");
+
+router.post("/register", validateBody(schemas.registerSchema), ctrlWrapper.register);
+
+router.post("/login", validateBody(schemas.loginSchema), ctrlWrapper.login);
+
+router.get('users/current', authentificate, ctrlWrapper.getCurrent);
+
+router.post('/logout', authentificate, ctrlWrapper.logout);
+
+router.patch('/users/avatars', authentificate, upload.single('avatar'), ctrlWrapper.updateAvatar)
+
+module.exports = router;
