7a1779ff5668907d580c4852634c1bdb522dd7f0
Max <geramaks03@gmail.com>
2023-10-10 13:10:27 +0300

    Fix

--- controllers/contacts-controllers.js
+++ controllers/contacts-controllers.js
@@ -30,7 +30,7 @@ const addContact = async (req, res) => {
 
 const removeContact = async (req, res) => {
   const { id: owner } = req.params;
-  const result = await Contact.findOne({_id: id, owner});
+  const result = await Contact.findByIdAndDelete({_id: id, owner});
   if (!result) {
     throw HttpError(404, "Not Found");
   }
@@ -39,7 +39,7 @@ const removeContact = async (req, res) => {
 
 const updateContactById = async (req, res) => {
   const { id: owner } = req.params;
-  const result = await Contact.findOne({_id: id, owner}, req.body, { new: true });
+  const result = await Contact.findByIdAndUpdate({_id: id, owner}, req.body, { new: true });
   if (!result) {
     throw HttpError(404, "Not Found");
   }
